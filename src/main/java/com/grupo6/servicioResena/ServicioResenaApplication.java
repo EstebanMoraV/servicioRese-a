57244dee1d3832d5101255287cea6b5da1503509
Esteban <esteban.morauc@gmail.com>
2025-05-26 20:42:16 -0400

    Creación del microservicio Reseña

---
+++ src/main/java/com/grupo6/servicioResena/ServicioResenaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioResena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioResenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioResenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo6/servicioResena/ServicioResenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioResena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioResenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
